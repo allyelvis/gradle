ad0400aaecc983ede48323903fbdb1aff04a9212
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-08-29 21:23:58 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+    deps = [
+      pkgs.dnscontrol
+      pkgs.gradle_5
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
